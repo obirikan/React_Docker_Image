bc1443c9f234e762fcf8b3dcc2a558a255303413
obirikan <obirikan020@gmail.com>
2024-02-20 23:48:18 +0100

    new updates

--- src/App.js
+++ src/App.js
@@ -8,7 +8,9 @@ const shareLink = "https://docker.com/";
 const App = () => {
   return (
     <div className="App">
+      
       <Confetti />
+
       <header className="App-header">
         <h1 style={{ marginBottom: "0px" }}>react Docker image</h1>
         <p style={{ marginTop: "10px", marginBottom: "50px" }}>
